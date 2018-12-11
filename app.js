669f9b792421c46e18be485a54e601e74cc632ea
giordifungula <giordifungula@gmail.com>
2018-12-10 22:56:39 -0800

    updated html content

--- app.js
+++ app.js
@@ -10,11 +10,13 @@ function Sticky() {
   if (window.pageYOffset > sticky) {
     header.classList.add("sticky");
     // When the screen is scrolled below header add the sticky class
-    header.style.opacity = '0.8';
+    header.style.opacity = '0.9';
+    header.style.background = '#eee';
     // also lighten the background picture
   } else {
     header.classList.remove("sticky");
     header.style.opacity = '1';
+    header.style.background = '#111';
     // if its scrolled back remove the class and the opacity
     
   }
